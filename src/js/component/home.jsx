562889137617ee415180b5355fcc1b28d85d1893
Wishmastering <charsdb@gmail.com>
2022-08-25 07:44:06 +0000

    1er Update

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,32 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, {useState} from "react";
+ 
 
 //create your first component
 const Home = () => {
+	const [todo,todoUpdate]=useState("")
+	const [list,listUpdate]=useState("")
+	
+	
+	// FUNCION SE LLAMA DENTRO DEL INPUT PARA QUE SE VEA MEJOR 
+	//function handleTodo(e){
+	// 	todoUpdate(e.target.value);
+	// }
+
+	function handleList(e){
+		listUpdate(e.target.value);
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+			<div className=" text-center">
+				<h3> ToDo List</h3>
+				<input type="text" 
+				placeholder="What needs to be done?" 
+				className="todo" 
+				onChange={(e)=>{todoUpdate(e.target.value)}}
+				eventKeypress={(e)=>{todoUpdate(e.target.value)}}
+				/>
+			</div>
+		
 	);
 };
 
